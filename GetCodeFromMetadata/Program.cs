10cdf562b7118efc9c493050ca5b8c5f26651033
Lukasz Urbanski <lukasz.urbanski@yahoo.pl>
2019-07-14 14:23:13 +0200

    Add project files.

---
+++ GetCodeFromMetadata/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetCodeFromMetadata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v1 = new AcmeCorp.Training.Services.
+        }
+    }
+}
